b1b5a357081ead75298481f491a6436ef9bf0796
Roberto Pedro <robertopedro100@gmail.com>
2026-09-04 19:30:22 -0300

    implementa fluxo controller

---
+++ src/main/java/br/com/petz/cliente_pet/application/api/ClienteController.java
@@ -0,0 +1,18 @@
+package br.com.petz.cliente_pet.application.api;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+public class ClienteController implements ClienteAPI {
+
+
+
+    @Override
+    public ClienteResponse postCliente(ClienteRequest clienteRequest) {
+        log.info("[inicia] ClienteController - postCliente");
+        log.info("[finaliza] ClienteController - postCliente");
+        return null;
+    }
+}
