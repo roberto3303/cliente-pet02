54d690ad8e9505c04b1cc677f9fe2eafa7ffb2c3
Roberto Pedro <robertopedro100@gmail.com>
2026-09-04 17:06:23 -0300

    implementa request e Response + corrige api

--- src/main/java/br/com/petz/cliente_pet/application/api/ClienteAPI.java
+++ src/main/java/br/com/petz/cliente_pet/application/api/ClienteAPI.java
@@ -1,9 +1,13 @@
 package br.com.petz.cliente_pet.application.api;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v1/cliente")
 public interface ClienteAPI {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    ClienteResponse postCliente(@RequestBody ClienteRequest clienteRequest);
 }
---
+++ src/main/java/br/com/petz/cliente_pet/application/api/ClienteResponse.java
@@ -0,0 +1,4 @@
+package br.com.petz.cliente_pet.application.api;
+
+public class ClienteResponse {
+}
