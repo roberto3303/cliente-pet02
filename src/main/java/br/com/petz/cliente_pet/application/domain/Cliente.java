7b58a4479e5a4ccf4e2c8a53dd7ad927b9173f73
Roberto Pedro <robertopedro100@gmail.com>
2026-09-04 16:48:59 -0300

    implementa domain + fluxo API

--- src/main/java/br/com/petz/cliente_pet/application/domain/Cliente.java
+++ src/main/java/br/com/petz/cliente_pet/application/domain/Cliente.java
@@ -1,16 +1,27 @@
 package br.com.petz.cliente_pet.application.domain;
 
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.UUID;
 
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Entity
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "idCliente", updatable = false, unique = true, nullable = false)
+    private UUID idCliente;
     @NotBlank
     private String nomeCompleto;
     @NotBlank
@@ -33,4 +44,16 @@ public class Cliente {
     private LocalDateTime dataHoraCadastro;
     private LocalDateTime dataHoraUltimaAlteracao;
 
+    public Cliente(String nomeCompleto, String email, String celular, String telefone,
+                   Sexo sexo, LocalDate dataNascimento, String cpf, Boolean aceitaTermos) {
+        this.nomeCompleto = nomeCompleto;
+        this.email = email;
+        this.celular = celular;
+        this.telefone = telefone;
+        this.sexo = sexo;
+        this.dataNascimento = dataNascimento;
+        this.cpf = cpf;
+        this.aceitaTermos = aceitaTermos;
+        this.dataHoraCadastro = LocalDateTime.now();
+    }
 }
